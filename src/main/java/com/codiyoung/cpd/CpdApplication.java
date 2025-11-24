5697c00a81ca9fce123bdc443d7e43f9aa4ecd50
teotaku <dongik9467@naver.com>
2025-11-24 21:39:29 +0900

    firstCommit

---
+++ src/main/java/com/codiyoung/cpd/CpdApplication.java
@@ -0,0 +1,13 @@
+package com.codiyoung.cpd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codiyoung/cpd/CpdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codiyoung.cpd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CpdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
